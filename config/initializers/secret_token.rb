cf7431b4ce323a1184c4582b5fa757be46f571ef
amadden80 <amadden80@gmail.com>
2014-01-11 15:06:08 -0500

    testful_restful_crud

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestfulRestfulCrud::Application.config.secret_token = '56370ede3bdd84ea1766bc4632b89ab46fc6de2992ef5940dfe67e94e3675c1f1016b44366e18e7dd643d6bd5f285484963ec3d83b30ea516bc100484628aef3'
